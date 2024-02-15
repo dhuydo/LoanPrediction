6f80fb4452ff3ab018929f1118f052be5791f40f
dhuydo <ncdhuy.jul04@gmail.com>
2024-02-15 13:44:15 +0700

    Modify port

--- application.py
+++ application.py
@@ -12,17 +12,14 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'GET':
-        return render_template('home.html')
-    else:
-        input = [instance for instance in request.form.values()]
-        data = GetData(input).get_data_asframe()
-        y_pred = PredictPipeline().predict(data)[:,1]
-        result = f'Loan Application approved: {(y_pred>=0.5)[0]}, with probability of being approved: {(y_pred)[0]:.4f}'
-        return render_template('home.html',result=result)
+    input = [instance for instance in request.form.values()]
+    data = GetData(input).get_data_asframe()
+    y_pred = PredictPipeline().predict(data)[:,1]
+    result = f'Loan Application approved: {(y_pred>=0.5)[0]}, with probability of being approved: {(y_pred)[0]:.4f}'
+    return render_template('home.html',result=result)
 
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=1001, debug=True)
+    app.run(host='0.0.0.0', port=1002, debug=True)
