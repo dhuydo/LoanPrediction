0482240b83ff11b547be8c34d3b795dc19c9f6cd
dhuydo <ncdhuy.jul04@gmail.com>
2024-02-15 14:06:30 +0700

    Modify port

--- application.py
+++ application.py
@@ -14,10 +14,10 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    input = [instance for instance in request.form.values()]
-    data = GetData(input).get_data_asframe()
-    y_pred = PredictPipeline().predict(data)[:,1]
-    result = f'Loan Application approved: {(y_pred>=0.5)[0]}, with probability of being approved: {(y_pred)[0]:.4f}'
+    result = [instance for instance in request.form.values()]
+    # data = GetData(input).get_data_asframe()
+    # y_pred = PredictPipeline().predict(data)[:,1]
+    # result = f'Loan Application approved: {(y_pred>=0.5)[0]}, with probability of being approved: {(y_pred)[0]:.4f}'
     return render_template('home.html',result=result)
 
 
